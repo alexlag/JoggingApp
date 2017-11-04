70027b59fdba0e7a83030ffa7bb3e7ca2a8dc544
alexlag <bo4onok@gmail.com>
2017-11-04 18:54:05 +0300

    full crud test

--- server/__tests__/api_test.js
+++ server/__tests__/api_test.js
@@ -4,6 +4,7 @@ const app = require('../api/index.js')
 const User = require('../models/user').default
 
 let token = null
+let recordId = null
 
 const user = {
   name: 'Test',
@@ -25,25 +26,25 @@ const record = {
 
 beforeAll(() => User.remove({}))
 
-describe('User API', () => {
-  test('It does not create user without email', async () => {
+describe('Users API', () => {
+  test('does not create user without email', async () => {
     const response = await request(app).post('/users/signup').send(user)
     expect(response.statusCode).toBe(400)
     expect(response.body.error).toBe(true)
   })
 
-  test('It does not create user with mismatching password', async () => {
+  test('does not create user with mismatching password', async () => {
     const response = await request(app).post('/users/signup').send({ ...user, password: 'none', email: 'test@foo.bar' })
     expect(response.statusCode).toBe(400)
     expect(response.body.error).toBe(true)
   })
 
-  test('It creates correct user', async () => {
+  test('creates correct user', async () => {
     const response = await request(app).post('/users/signup').send(correctUser)
     expect(response.statusCode).toBe(200)
   })
 
-  test('It authorizes user correctly', async () => {
+  test('authorizes user correctly', async () => {
     const response = await request(app).post('/users/signin').send(correctUser)
     token = response.body.token
     expect(response.statusCode).toBe(200)
@@ -51,22 +52,76 @@ describe('User API', () => {
 })
 
 describe('Records API', () => {
-  test('It should not allow anauth', async () => {
+  test('should not allow anauth', async () => {
     const response = await request(app).get('/records')
     expect(response.statusCode).toBe(401)
   })
 
-  test('It shows list', async () => {
+  test('shows list', async () => {
     const response = await request(app).get('/records').set('Authorization', `BEARER ${token}`)
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual([])
   })
 
-  test('It creates new record', async () => {
+  test('creates new record', async () => {
     const response = await request(app)
       .post('/records')
       .send(record)
       .set('Authorization', `BEARER ${token}`)
     expect(response.statusCode).toBe(200)
+  })
+
+  test('creates duplicate record', async () => {
+    const response = await request(app)
+      .post('/records')
+      .send(record)
+      .set('Authorization', `BEARER ${token}`)
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('gets record', async () => {
+    let response = await request(app)
+      .get('/records')
+      .set('Authorization', `BEARER ${token}`)
+    expect(response.statusCode).toBe(200)
+    const records = response.body
+
+    recordId = records[0]._id
+
+    response = await request(app)
+      .get(`/records/${recordId}`)
+      .set('Authorization', `BEARER ${token}`)
+    expect(response.statusCode).toBe(200)
+    expect(response.body._id).toBe(recordId)
+    expect(response.body.distance).toBe(record.distance)
+    expect(response.body.time).toBe(record.time)
+  })
+
+  test('updates record', async () => {
+    let response = await request(app)
+      .put(`/records/${recordId}`)
+      .send({ distance: 10000 })
+      .set('Authorization', `BEARER ${token}`)
+    expect(response.statusCode).toBe(200)
+
+    response = await request(app)
+      .get(`/records/${recordId}`)
+      .set('Authorization', `BEARER ${token}`)
+    expect(response.statusCode).toBe(200)
+    expect(response.body._id).toBe(recordId)
+    expect(response.body.distance).toBe(10000)
+    expect(response.body.time).toBe(record.time)
+  })
+
+  test('deletes record', async () => {
+    let response = await request(app)
+      .delete(`/records/${recordId}`)
+      .set('Authorization', `BEARER ${token}`)
+    expect(response.statusCode).toBe(200)
+
+    response = await request(app)
+      .get(`/records/${recordId}`)
+      .set('Authorization', `BEARER ${token}`)
+    expect(response.statusCode).toBe(404)
   })
 })
